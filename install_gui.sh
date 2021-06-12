3503435240c6226863250334393faba47af47363
jhye0n <stjhyeon@kakao.com>
2021-06-12 14:00:30 +0900

    script updated - 210612_3

--- install_gui.sh
+++ install_gui.sh
@@ -13,9 +13,9 @@ sudo add-apt-repository \
 $(lsb_release -cs) \
 stable"
 
-sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 sudo apt update
 
